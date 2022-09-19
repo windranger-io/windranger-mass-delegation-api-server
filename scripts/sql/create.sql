6a3ddf34f0c22dee2ede01f66201cadcc89cabeb
CJ <CjHare@users.noreply.github.com>
2022-09-19 15:01:19 +1000

    trimming code

--- scripts/sql/create.sql
+++ scripts/sql/create.sql
@@ -1,59 +1,41 @@
-CREATE TABLE delegatees
+-- Delegatees are account that receive delegation of voting power from Delegators
+CREATE TABLE Delegation
 (
-    token_address          CHAR(20),
-    delegator_address      CHAR(20),
-    delegatee_address      CHAR(20),
-    proof                  VARCHAR,
-    delegated_weight       BIGINT,
-    delegated_block_number BIGINT
+    token_address     CHAR(20),
+    delegator_address CHAR(20),
+    delegatee_address CHAR(20),
+    proof             VARCHAR,
+    delegated_weight  BIGINT,
+    delegated_block   BIGINT,
+    PRIMARY KEY (token_address,
+                 delegator_address,
+                 delegatee_address)
 );
-
-CREATE UNIQUE INDEX delegatees_index
-    ON delegatees (
-                   token_address,
-                   delegator_address,
-                   delegatee_address
-        );
-
-CREATE INDEX delegatees_index_delegator
-    ON delegatees (
+CREATE INDEX Delegation_index_delegator
+    ON Delegation (
                    token_address,
                    delegator_address
         );
 
-CREATE INDEX delegatees_index_delegatee
-    ON delegatees (
+CREATE INDEX Delegation_index_delegatee
+    ON Delegation (
                    token_address,
                    delegatee_address
         );
 
-
-
-CREATE TABLE delegators
+-- Delegators delegate their voting power to Delegatees
+CREATE TABLE Delegators
 (
     token_address     CHAR(20),
     delegator_address CHAR(20),
     trie_root         CHAR(32),
-    delegated_block   BIGINT
+    delegated_block   BIGINT,
+    PRIMARY KEY (token_address, delegator_address)
 );
-
-CREATE UNIQUE INDEX delegators_index_delegator
-    ON delegators (
-                   token_address,
-                   delegator_address
-        );
 
 
 CREATE TABLE Token
 (
-    CHAR(20) token_address UNIQUE,
-    PRIMARY KEY,
+    CHAR(20) token_address PRIMARY KEY,
     CHAR(20) deployed_instance
-)
-
-
-CREATE TABLE tokens
-(
-    token_address     CHAR(20) UNIQUE PRIMARY KEY,
-    deployed_instance CHAR(20)
 );
