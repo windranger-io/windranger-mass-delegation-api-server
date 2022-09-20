e8298e377e46247d512696624e70d324b989f557
CJ Hare <CjHare@users.noreply.github.com>
2022-09-20 10:02:31 +1000

    feat: Sql creation script (#5)

---
+++ scripts/sql/create.sql
@@ -0,0 +1,34 @@
+-- Delegatees are account that receive delegation of voting power from Delegators
+CREATE TABLE Delegation
+(
+    token_address     CHAR(20),
+    delegator_address CHAR(20),
+    delegatee_address CHAR(20),
+    proof             TEXT,
+    delegated_weight  BIGINT,
+    delegated_block   BIGINT,
+    PRIMARY KEY (token_address,
+                 delegator_address,
+                 delegatee_address)
+);
+CREATE INDEX Delegation_index_delegator
+    ON Delegation (
+                   token_address,
+                   delegator_address
+        );
+
+CREATE INDEX Delegation_index_delegatee
+    ON Delegation (
+                   token_address,
+                   delegatee_address
+        );
+
+-- Delegators delegate their voting power to Delegatees
+CREATE TABLE Delegators
+(
+    token_address     CHAR(20),
+    delegator_address CHAR(20),
+    trie_root         CHAR(32),
+    delegated_block   BIGINT,
+    PRIMARY KEY (token_address, delegator_address)
+);
