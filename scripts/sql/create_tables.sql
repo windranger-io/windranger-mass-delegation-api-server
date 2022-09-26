6b5a0ab7e4dabece9b20cc16a970441faec31998
CJ Hare <CjHare@users.noreply.github.com>
2022-09-26 11:36:37 +1000

    fix: Postgresql syntax (#16)

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -6,29 +6,30 @@ CREATE TABLE delegation
     proof             TEXT,
     delegated_weight  BIGINT,
     delegated_block   BIGINT,
-    PRIMARY KEY (token_address,
-                 delegator_address,
+    PRIMARY KEY (delegator_address,
                  delegatee_address)
 );
 CREATE INDEX delegation_index_delegator
     ON delegation (
-                   token_address,
                    delegator_address
+        );
+CREATE INDEX delegation_index_delegatee
+    ON delegation (
+                   delegatee_address
         );
 
 -- Delegators delegate their voting power to Delegatees
 CREATE TABLE delegators
 (
-    delegator_address CHAR(20),
+    delegator_address CHAR(20) PRIMARY KEY,
     trie_root         CHAR(32),
-    delegated_block   BIGINT,
-    PRIMARY KEY delegator_address
+    delegated_block   BIGINT
 );
 
 -- The single supported token's address, with support for contract migration
 CREATE TABLE token
 (
-    contract_address        CHAR(20),
+    contract_address        CHAR(20) PRIMARY KEY,
     delegation_from_block   BIGINT,
     delegated_to_block      BIGINT
 );
